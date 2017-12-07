d424e0ce4b580d00878e64d499826fed1ea97474
Hao <wanghao8522@gmail.com>
2017-12-08 11:10:14 +1300

    Database connection
    -database connection
    - create Entityframe helper class

---
+++ Hao_CRUD_APP_1/Hao_CRUD_APP_1/Service/EFDBHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Hao_CRUD_APP_1.Service
+{
+
+    /// <summary>
+    /// entityframe work public access class
+    /// </summary>
+    public class EFDBHelper
+    {
+        private DbContext dbContext = null;
+
+        public EFDBHelper(DbContext context)
+        {
+            this.dbContext = context;
+        }
+
+
+        public int Add<T>(T entity) where T : class
+        {
+
+            dbContext.Entry<T>(entity).State = EntityState.Added;
+
+            return dbContext.SaveChanges();
+
+        }
+
+
+        public int Modify<T>(T entity)where T : class
+        {
+            dbContext.Entry<T>(entity).State = EntityState.Modified;
+            return dbContext.SaveChanges();
+        }
+
+
+        public int Delete<T>(T entity) where T : class
+        {
+            dbContext.Entry<T>(entity).State = EntityState.Deleted;
+            return dbContext.SaveChanges();
+        }
+    }
+}
